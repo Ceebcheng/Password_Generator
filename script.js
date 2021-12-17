dd42c26c08e5499fb7f9f6195154e847dea74802
Ceebcheng <jonathan2075@gmail.com>
2021-12-16 22:44:11 -0600

    first commit

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var charLow = "abcdefghijklmnopqrstuvwxyz";
-var charUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var charNum = "0123456789";
-var charSpec = "~`!@#$%^&*()_-+={[}]|\:;'<,>.?/";
-var passwordMaxLength = 128;
+var charNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var charSpec = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~", "=", "@", "^"];
+var charLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var charUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 
 // Write password to the #password input
 
 function generatePassword() {
-  charLength = (prompt("How long do you want your password to be from 8 - 128 Characters"));
- 
-  while(charLength >= 8 && charLength <= 128) {
-    var charLength = (prompt("How many characters would you like your password to contain?"));
-    } 
-    charLow = confirm("If you want Lower Case characters click OK ");
-    charUp = confirm("If you want Upper Case characters click OK ");    
-    charNum = confirm("If you want Number characters click OK ");
-    charSpec = confirm("If you want Special characters click OK ");
+  var charLength = (prompt("How long do you want your password to be from 8 - 128 Characters"));
+    while(charLength < 8 || charLength > 128) {
+      alert("Try again");
+      charLength = (prompt("How long do you want your password to be from 8 - 128 Characters"));
   }
+  var confirmcharLow = confirm("If you want Lower Case characters click OK ");
+  var confirmcharUp = confirm("If you want Upper Case characters click OK ");    
+  var confirmcharNum = confirm("If you want Number characters click OK ");
+  var confirmcharSpec = confirm("If you want Special characters click OK ");
+    while(confirmcharUp === false && confirmcharLow === false && confirmcharSpec === false && confirmcharNum === false) {
+        alert("You must choose at least one parameter");
+        confirmcharLow = confirm("If you want Lower Case characters click OK ");
+        confirmcharUp = confirm("If you want Upper Case characters click OK ");    
+        confirmcharNum = confirm("If you want number characters click OK ");
+        confirmcharSpec = confirm("If you want Special characters click OK ");  
+  }
+  var passwordChar = []
+
+  if (confirmcharLow) { passwordChar = passwordChar.concat(charLow) }
+  if (confirmcharUp) { passwordChar = passwordChar.concat(charUp) }
+  if (confirmcharNum) { passwordChar = passwordChar.concat(charNum) }
+  if (confirmcharSpec) { passwordChar = passwordChar.concat(charSpec) }
+
+  var randomPassword;
+      
+      for (var i = 0; i < charLength; i++) {
+        randomPassword = randomPassword + passwordChar[Math.floor(Math.random() * passwordChar.length)];
+      }
+      return randomPassword;
+    
+}
 
 function writePassword() {
   var password = generatePassword();
